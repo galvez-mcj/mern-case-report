aa26867b46ff0dcec7fa5d7de8f0914cfac41091
tinay <galvez.mcj@gmail.com>
2023-03-31 10:03:25 +0800

    setup express

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,20 @@
+require('dotenv').config()
+
 const express = require('express')
 
 const app = express()
 
+// middleware
+app.use((req, res, next) => {
+    console.log(`path: ${req.path} | method: ${req.method}`)
+    next()
+})
 
-app.listen(3000, () => {
-    console.log('listening on port 3000')
+app.get('/', (req, res) => {
+    res.json({ message: 'Welcome to the app' })
+})
+
+app.listen(process.env.PORT, () => {
+    console.log(`listening on port ${process.env.PORT}`)
 });
+
